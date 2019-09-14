17ca56172e9cbdfb7543bebeb73b2b6d4b0b7117
Alyssa Yates <bosslys@icloud.com>
2019-09-14 17:40:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = Hash.new {:yukihiro_matsumoto => {
+ Hash.new {:yukihiro_matsumoto => {
 	:known_for => "Ruby",
 	:languages => ["LISP", "C"]
 	} }
